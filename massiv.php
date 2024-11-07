9bdfa460234e01f843e2eaa23d4c2bceaed8fa01
Konstantin <rilekt11@gmail.com>
2024-11-08 02:40:08 +0500

    instal massiv

---
+++ massiv.php
@@ -0,0 +1,123 @@
+<?php
+
+//задание3
+
+
+
+//задание2
+//$arr9 = [];
+//for ($b = 0; $b < 10; $b++) {
+//    $arr9[$b] = mt_rand(0, 10);
+//    $arr10 = array_reverse($arr9);
+//}
+//print_r($arr9);
+//
+//echo '<br>';
+//print_r($arr10);
+
+
+
+//задание1
+//$arr8 =[50, 45, 40, 35, 30];
+//$result = array_sum($arr8);
+//print_r($result);
+
+
+
+
+
+//$arr6 = [[10, 11, 12],[21, 22], [31, 32, 33]];
+//echo '<pre>';
+//print_r($arr6);
+//echo '</pre>';
+//$arr7 = [
+//  'fruits' => [
+//      'apple' => 'яблоко',
+//      'banana' => 'банан',
+//      'pineapple' => 'ананас'
+//  ],
+//  'vegetables' =>   [
+//      'tomato' => 'помидор',
+//      'cucumber' => 'огурец'
+//  ],
+//   'drinks' => [
+//       'water' => 'вода',
+//       'coffee' => 'кофе',
+//       'tea' => 'чай'
+//   ]
+//];
+//foreach ($arr7 as $key => $value){
+//    foreach ($value as $key_new => $item){
+//        echo $item . '<br>';
+//    }
+//}
+
+
+
+
+
+
+//echo '<pre>';
+//print_r($arr7);
+//echo '</pre>';
+
+
+
+
+
+
+//$user['name'] = 'Tanya';
+//$user['surname'] = 'Ivanova';
+//$user['age'] = '30';
+//$user['children'] = '1';
+//print_r($user);
+//$arr4 = array(
+//    'hello' => 'привет',
+//    'bay' => 'пока',
+//);
+//echo '<pre>';
+//
+//print_r($arr4);
+//
+//echo '</pre>';
+//
+//$arr5 = [
+//    'apple' => 'яблоко',
+//    'banana' => 'банан',
+//    'pineapple' => 'ананас',
+//];
+//print_r($arr5);
+//echo '<br>';
+//foreach ($arr5 as $key => $value)
+//{
+//    echo $key . ' = ' . $value . '<br>';
+//}
+
+
+
+
+//$arr1 = array('яблоко', 'банан', 'ананас');
+//echo count($arr1);
+//print_r($arr1);
+//$arr2[] = 'tomat';
+//$arr2[] = 'cucumber';
+//print_r($arr2);
+//echo '<br>';
+//$arr3 = ['яблоко', 'банан', 'ананас'];
+//$arr3[] = 'grusha';
+////echo $arr3[1];
+//$arr3[3] = 'voda';
+//$arr3[] = 'chay';
+//print_r($arr3);
+//echo '<br>';
+//$arr3[] = 'coffe';
+//$i = mt_rand(0,count($arr3)-1);
+//echo $arr3[$i];
+/*for ($i = 0; $i < count($arr3); $i++)
+{
+    echo $arr3[$i] . '<br>';
+}*/
+//foreach ($arr3 as $value)
+//{
+//    echo $value . '<br>';
+//}
